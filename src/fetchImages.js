d816d3e76f5a8285a222cc76bd9bd84927d0b522
Artem Riabchych <artemskyart@gmail.com>
2023-01-16 02:00:08 +0200

    all-done

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,11 +1,12 @@
+import axios from 'axios';
 export async function fetchImages(name, page) {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const API_KEY = '32854135-9d23b52454b3142f3a14ae48f'
-    const resp = await fetch(
+  const BASE_URL = 'https://pixabay.com/api/';
+  const API_KEY = '32854135-9d23b52454b3142f3a14ae48f';
+  const resp = await axios.get(
     `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
   );
-  if (!resp.ok) {
-    throw new Error(resp.statusText);
-  }
-  return await resp.json();
+  // if (!resp.ok) {
+  //   throw new Error(resp.statusText);
+  // }
+  return await resp.data;
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import './sass/styles.scss';
 import { fetchImages } from './fetchImages';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const options = {
-root: null,
-rootMargin: '300px',
-threshold: 1.0
-}
-const observer = new IntersectionObserver (onInfinityLoad, options);
+  root: null,
+  rootMargin: '300px',
+  threshold: 1.0,
+};
+const observer = new IntersectionObserver(onInfinityLoad, options);
 
 let page = 1;
 
-let gallery = new SimpleLightbox(".gallery a", {
-  captionsData: "alt",
-  captionDelay: 250
-});
-
 const refs = {
-   form: document.querySelector('.search-form'),
-   input: document.querySelector('.search-input'),
-   list: document.querySelector('.gallery'),
-   guard: document.querySelector('.js-guard')
- };
+  form: document.querySelector('.search-form'),
+  input: document.querySelector('.search-input'),
+  list: document.querySelector('.gallery'),
+  guard: document.querySelector('.js-guard'),
+};
 
-refs.form.addEventListener('submit',onInputName);
+refs.form.addEventListener('submit', onInputName);
 
 function onInputName(evt) {
   evt.preventDefault();
@@ -43,14 +38,17 @@ function onInputName(evt) {
           `Sorry, there are no images matching your search query. Please try again.`
         );
         return;
-      } else { Notify.success(`Hooray! We found ${data.totalHits} images.`);}
-      
-     const markup = createMarkup(data);
-     newMarkup (markup);
+      } else {
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+      }
+
+      const markup = createMarkup(data);
+      newMarkup(markup);
+      lightbox.refresh();
       observer.observe(refs.guard);
     })
     .catch(createErrorMessage);
-    // .catch(err=>console.error(err));
+  // .catch(err=>console.error(err));
 }
 
 function createMarkup(data) {
@@ -64,64 +62,73 @@ function createMarkup(data) {
       comments,
       downloads,
     }) => `   
-    <div class="photo-card gallery__image">
-    <a class="gallery__item" href="${largeImageURL}">
-  <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
-  <div class="info">
-    <p class="info-item">
-      <b>Likes</b><br>${likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b><br>${views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b><br>${comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b><br>${downloads}
-    </p>
-  </div>
-</div>`
+    <div class="photo-card">
+      <a href="${largeImageURL}">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          <br>${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          <br>${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          <br>${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          <br>${downloads}
+        </p>
+      </div>
+    </div>`
   );
   return markup.join('');
 }
 
-function newMarkup(markup){
+function newMarkup(markup) {
   refs.list.innerHTML = markup;
 }
 
-function addMarkup(markup){
- refs.list.insertAdjacentHTML('beforeend', markup);
+function addMarkup(markup) {
+  refs.list.insertAdjacentHTML('beforeend', markup);
 }
 
 function createErrorMessage(err) {
-  // refs.list.innerHTML = '';
-  // refs.info.innerHTML = '';
-  // refs.input.value = ""
-  Notify.failure(
-    `${err}`
-  );
+  Notify.failure(`${err}`);
 }
 
-function onInfinityLoad (entries){
-// console.log(entries)
-entries.forEach(entry => {
-  if (entry.isIntersecting){
-    page+=1
-    fetchImages(refs.input.value, page)
-    .then(data => {
-           
-     const markup = createMarkup(data);
-     addMarkup (markup);
-     console.log(data.totalHits/40,"-",page, '', data.totalHits/40 <= page);
-     if(data.totalHits/40 <= page){
-      observer.unobserve(refs.guard);
-      Notify.success(`We're sorry, but you've reached the end of search results.`)
-     }
-     
-    })
-    .catch(createErrorMessage);
-  }
-});
+function onInfinityLoad(entries) {
+    entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      page += 1;
+      fetchImages(refs.input.value, page)
+        .then(data => {
+          const markup = createMarkup(data);
+          addMarkup(markup);
+          lightbox.refresh();
+          if (data.totalHits / 40 <= page) {
+            observer.unobserve(refs.guard);
+            Notify.success(
+              `We're sorry, but you've reached the end of search results.`
+            );
+          }
+        })
+        .catch(createErrorMessage);
+    }
+  });
 }
 
+const lightbox = new SimpleLightbox('.gallery a');
+
+// Плавного скрола я не зрозумів
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
